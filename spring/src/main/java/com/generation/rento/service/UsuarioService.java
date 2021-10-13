e54ba672da143d32dfa406c667055a738ba6a448
oGraebin <felipegraebin@hotmail.com>
2021-10-13 15:28:29 -0300

    :bug: bug fixed

--- spring/src/main/java/com/generation/rento/service/UsuarioService.java
+++ spring/src/main/java/com/generation/rento/service/UsuarioService.java
@@ -40,6 +40,7 @@ public class UsuarioService {
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
 				user.get().setSenha(usuario.get().getSenha());
+				user.get().setTipoCadastro(usuario.get().getTipoCadastro());
 
 				return user;
 			}
